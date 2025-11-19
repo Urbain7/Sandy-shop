d622620e133d365ed8f4d5d6b00f13445c0d8f57
mfrx <99825978+Urbain7@users.noreply.github.com>
2025-11-19 14:36:13 +0000

    Update theme.js

--- js/theme.js
+++ js/theme.js
@@ -25,3 +25,9 @@ mobileNav.querySelectorAll('a').forEach(link => {
         mobileNav.classList.remove('open');
     });
 });
+const currentPage = window.location.pathname.split('/').pop() || 'index.html';
+document.querySelectorAll('nav a').forEach(link => {
+    if (link.getAttribute('href') === currentPage) {
+        link.classList.add('active');
+    }
+});
